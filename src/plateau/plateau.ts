e080bd3680be209360fe15fc361a94ad0d4adfc5
TimalkaK <timalkamk@gmail.com>
2023-08-01 20:33:13 +0100

    Feat: Adjusted test to check perimeter coordinates of Plateau

--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -1,34 +1,21 @@
 type Grid = { x: number, y: number };
+const minCoord: Grid = {x:0, y:0};
+const upperLeftCoord: Grid = {x:5, y:0};
+const lowerRightCoord: Grid = {x:0, y:5};
+const maxCoord: Grid = {x:5,y:5};
 
-export function upperRight(x: number, y: number): boolean{
 
-  const maxCoordinates: Grid = {x:5,y:5};
-
-  if (x === maxCoordinates.x && y === maxCoordinates.y){
-    //console.log(maxCoordinates);
-    return true;
+export function checkPlateauPerimeter(x: number, y: number): string | false{
+  if (x === maxCoord.x && y === maxCoord.y){
+    return "5,5";
+  }else if (x === upperLeftCoord.x && y === upperLeftCoord.y){
+    return "5,0";
+  }else if (x === lowerRightCoord.x && y === lowerRightCoord.y){
+    return "0,5";
+  }else if (x === minCoord.x && y === minCoord.y){
+    return "0,0";
   }else{
-    return false;
+    return "Out of Plateau Perimeter"
   }
 }
 
-export function upperLeft(x: number, y: number): boolean {
-
-  const minCoordinates: Grid = {x:0, y:5};
-
-  return true ;
-}
-
-export function lowerRight(x: number, y: number): boolean {
-
-  const minCoordinates: Grid = {x:5, y:0};
-
-  return true ;
-}
-
-export function lowerLeft(x: number, y: number): boolean {
-
-  const minCoordinates: Grid = {x:0, y:0};
-
-  return true ;
-}
--- tests/plateau.test.ts
+++ tests/plateau.test.ts
@@ -1,9 +1,19 @@
-import { upperRight } from "../src/plateau/plateau";
+import { checkPlateauPerimeter } from "../src/plateau/plateau";
 
-describe("test upperRight function", () => {
-  it("should return max co-ordinate true for upperRight(5,5)", () => {
-    expect(upperRight(5, 5)).toBe(true);
-  }); it("should return false for upperRight(2,3)", () => {
-    expect(upperRight(2, 3)).toBe(false);
+describe("test checkPlateauPerimeter function", () => {
+  it("should return max co-ordinates 5,5 for checkPlateauPerimeter(5,5)", () => {
+    expect(checkPlateauPerimeter(5, 5)).toBe("5,5");
+  }); it("should upper left co-ordinates for checkPlateauPerimeter(5,0)", () => {
+    expect(checkPlateauPerimeter(5, 0)).toBe("5,0");
   });
+  it("should return upper right co-ordinates for checkPlateauPerimeter(0,5)", () => {
+    expect(checkPlateauPerimeter(0, 5)).toBe("0,5");
+  });
+  it("should return min co-ordinates for checkPlateauPerimeter(0,0)", () => {
+    expect(checkPlateauPerimeter(0, 0)).toBe("0,0");
+  });
+  it("should return false for checkPlateauPerimeter(2,3)", () => {
+    expect(checkPlateauPerimeter(2, 3)).toBe("Out of Plateau Perimeter");
+  });
+  
 });
