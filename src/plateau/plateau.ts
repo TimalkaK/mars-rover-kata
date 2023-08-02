bf4a5954a8e7d513c50749a47d67f68ae3f943af
TimalkaK <timalkamk@gmail.com>
2023-08-02 19:55:01 +0100

    Refactored checkPlateauPerimeter to isInPlateauPerimeter

--- src/plateau/plateau.ts
+++ src/plateau/plateau.ts
@@ -5,19 +5,26 @@ const lowerRightCoord: Grid = {x:0, y:5};
 const maxCoord: Grid = {x:5,y:5};
 
 
-export function checkPlateauPerimeter(x: number, y: number): string | undefined {
-  if (x === maxCoord.x && y === maxCoord.y){
-    return "5,5";
-  }else if (x === upperLeftCoord.x && y === upperLeftCoord.y){
-    return "5,0";
-  }else if (x === lowerRightCoord.x && y === lowerRightCoord.y){
-    return "0,5";
-  }else if (x === minCoord.x && y === minCoord.y){
-    return "0,0";
-  }else if (x < 0 || x > 5 || y < 0 || y > 5){
-    return "Out of the Plateau Perimeter";
+// export function checkPlateauPerimeter(x: number, y: number): string | undefined {
+//   if (x === maxCoord.x && y === maxCoord.y){
+//     return "5,5";
+//   }else if (x === upperLeftCoord.x && y === upperLeftCoord.y){
+//     return "5,0";
+//   }else if (x === lowerRightCoord.x && y === lowerRightCoord.y){
+//     return "0,5";
+//   }else if (x === minCoord.x && y === minCoord.y){
+//     return "0,0";
+//   }else if (x < 0 || x > 5 || y < 0 || y > 5){
+//     return "Out of the Plateau Perimeter";
+//   }else{
+//     return `Co-ordinates ${x},${y} are within the Plateau`
+//   }
+// }
+
+export function isInPlateauPerimeter(x: number, y: number): boolean {
+  if (x < 0 || x > 5 || y < 0 || y > 5){
+    return false;
   }else{
-    return `Co-ordinates ${x},${y} are within the Plateau`
+    return true;
   }
 }
-
--- tests/plateau.test.ts
+++ tests/plateau.test.ts
@@ -1,34 +1,33 @@
-import { checkPlateauPerimeter } from "../src/plateau/plateau";
+import { isInPlateauPerimeter } from "../src/plateau/plateau";
 
-describe("test checkPlateauPerimeter function", () => {
-  it("should return max co-ordinates 5,5", () => {
-    expect(checkPlateauPerimeter(5,5)).toBe("5,5");
-  }); it("should upper left co-ordinates", () => {
-    expect(checkPlateauPerimeter(5,0)).toBe("5,0");
-  });
-  it("should return upper right co-ordinates", () => {
-    expect(checkPlateauPerimeter(0,5)).toBe("0,5");
-  });
-  it("should return min co-ordinates", () => {
-    expect(checkPlateauPerimeter(0,0)).toBe("0,0");
+describe("test isInPlateauPerimeter function", () => {
+  it("should return out of plateau perimeter", () => {
+    expect(isInPlateauPerimeter(-1,3)).toBe(false);
   });
   it("should return out of plateau perimeter", () => {
-    expect(checkPlateauPerimeter(-1,3)).toBe("Out of Plateau Perimeter");
+    expect(isInPlateauPerimeter(2,-3)).toBe(false);
   });
   it("should return out of plateau perimeter", () => {
-    expect(checkPlateauPerimeter(2,-3)).toBe("Out of Plateau Perimeter");
+    expect(isInPlateauPerimeter(6,-2)).toBe(false);
   });
   it("should return out of plateau perimeter", () => {
-    expect(checkPlateauPerimeter(6,-2)).toBe("Out of Plateau Perimeter");
-  });
-  it("should return out of plateau perimeter", () => {
-    expect(checkPlateauPerimeter(2,7)).toBe("Out of Plateau Perimeter");
+    expect(isInPlateauPerimeter(2,7)).toBe(false);
   });
   it("should return coordinates within plateau", () => {
-    expect(checkPlateauPerimeter(2,3)).toBe("Co-ordinates 2,3 are within the Plateau");
+    expect(isInPlateauPerimeter(2,3)).toBe(true);
   });
   it("should return coordinates within plateau", () => {
-    expect(checkPlateauPerimeter(4,4)).toBe("Co-ordinates 4,4 are within the Plateau");
+    expect(isInPlateauPerimeter(4,4)).toBe(true);
+  });  it("should return coordinates within plateau", () => {
+    expect(isInPlateauPerimeter(5,5)).toBe(true);
+  }); it("should return coordinates within plateau", () => {
+    expect(isInPlateauPerimeter(5,0)).toBe(true);
+  });
+  it("should return coordinates within plateau", () => {
+    expect(isInPlateauPerimeter(0,5)).toBe(true);
+  });
+  it("should return coordinates within plateau", () => {
+    expect(isInPlateauPerimeter(0,0)).toBe(true);
   });
   
 });
