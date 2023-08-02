a2ecf3f697e2a7202e8758d6ce8da54704ebc8c9
TimalkaK <timalkamk@gmail.com>
2023-08-02 14:14:49 +0100

    Feat: Added rotateRover function and its failing test

--- src/rover/rover.ts
+++ src/rover/rover.ts
@@ -1,9 +1,22 @@
 type Direction = "N" | "E" | "S" | "W";
 type Rover = {x: number, y: number, orientation: Direction};
 
-const rover1: Rover = {x:0, y:0, orientation:"N"};
+let rover1: Rover = {x:0, y:0, orientation:"N"};
 
-export function checkRoverPostion(): string {
-  console.log(rover1);
+export function checkRoverPostion(coordX: number, coordY: number , o: Direction): string {
+
+  rover1 = {x: coordX, y: coordY, orientation: o};
+
   return `${rover1.x} ${rover1.y} ${rover1.orientation}`;
 }
+
+type Instruction = "L" | "R" | "M";
+
+// function to rotate rover
+export function rotateRover(instruction : Instruction): string{
+
+  return "Blah";
+
+}
+
+// function to move rover 
--- tests/rover.test.ts
+++ tests/rover.test.ts
@@ -1,9 +1,18 @@
-import { checkRoverPostion } from "../src/rover/rover";
+import { rotateRover,checkRoverPostion } from "../src/rover/rover";
 
 describe("test checkRoverPostion function", () => {
-  it("should return true ", () => {
-    expect(checkRoverPostion()).toBe("0 0 N");
-  }); it("should return true ", () => {
-    expect(checkRoverPostion()).toBe("0 0 N");
+  it("should return co-ordinates ", () => {
+    expect(checkRoverPostion(0,0,"N")).toBe("0 0 N");
+  }); it("should return co-ordinates ", () => {
+    expect(checkRoverPostion(1,2,"E")).toBe("1 2 E");
+  });
+});
+
+
+describe("test rotateRover function", () => {
+  it("should return  ", () => {
+    expect(rotateRover("M")).toBe("Not a rotation instruction");
+  }); it("should return co-ordinates ", () => {
+    expect(rotateRover("R")).toBe("Not a rotation instruction");
   });
 });
